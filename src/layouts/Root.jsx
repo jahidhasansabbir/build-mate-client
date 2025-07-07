3c687644ceee3279663b30e9f53ca72ca50be655
jahidhasansabbir1 <jahidhasansabbir1@gmail.com>
2025-07-08 01:46:46 +0600

    added footer

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import Navbar from '../pages/shared/Navbar/Navbar';
+import Footer from '../pages/shared/Footer/Footer';
 
 const Root = () => {
     return (
         <div className="min-h-screen w-full bg-gradient-to-br from-[#0d1117c6] via-[#0d11174a] to-[#0d1117e3] text-[#F3F4F6]">
             <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
